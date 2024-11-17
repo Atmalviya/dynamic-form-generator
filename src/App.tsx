04e591e67d529aef44252c339938372a9482760a
atmalviya <atulmalviyav2@gmail.com>
2024-11-17 23:57:35 +0530

    react init boilerplate code

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-red-500 p-4 bg-gray-200 flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-red-300">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
